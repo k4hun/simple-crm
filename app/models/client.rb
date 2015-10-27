9e1e2b7eff27121666abd5d1524c6d399818a6e6
k4hun <bart.kaczor@gmail.com>
2015-10-27 20:35:00 +0000

    small changes in client model and views

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,9 +4,11 @@ class Client < ActiveRecord::Base
     
     accepts_nested_attributes_for :address
     
-    validates :firstname, :lastname, :nip, presence: { message: 'Pole nie może być puste' }
-    validates :nip, uniqueness: true
+    validates :firstname, :lastname, :nip, :email, presence: { message: 'Pole nie może być puste' }
+    validates :email, format: { with: (/\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/), message: 'Zły format adresu email!' }
+    validates :nip, uniqueness: { message: 'Nr NIP znajduje się już w bazie' }
     validates :nip, length: { is: 10 }
+    validates :nip, format: { with: /\A\d{10}\z/, message: 'NIP musi składać się z 10 cyfr' }
 
     def full_name
     	return "#{self.firstname} #{self.lastname}"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151021204833) do
+ActiveRecord::Schema.define(version: 20151027195848) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "street"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20151021204833) do
     t.boolean  "visible",               default: false
     t.datetime "created_at",                            null: false
     t.datetime "updated_at",                            null: false
+    t.string   "email"
   end
 
   create_table "companies", force: :cascade do |t|
